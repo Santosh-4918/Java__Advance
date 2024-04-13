2c42093c2c39c1cd6aacfd11f2c5c2494637842e
Santosh Kumar <santoshleo9z@gmail.com>
2024-04-13 19:22:58 +0530

    InsertionSort

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,20 +3,17 @@ public class SelectionSort {
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
-            // int minIndex = i;
+            int minIndex = i;
             for (int j = i + 1; j < n; j++) {
                 // for ascending order
-                if (arr[j] < arr[i]) {
-                    // minIndex = j;
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
             // Swap arr[i] and arr[minIndex]
-            // int temp = arr[i];
-            // arr[i] = arr[minIndex];
-            // arr[minIndex] = temp;
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
 
         }
 
